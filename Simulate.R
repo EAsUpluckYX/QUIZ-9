2e8c2e9bcf93011d197c582e8d37d565a864c4b3
Shuyuan Zheng <127867140+EAsUpluckYX@users.noreply.github.com>
2024-03-13 20:52:13 -0400

    Create Simulate.R

---
+++ Simulate.R
@@ -0,0 +1,22 @@
+set.seed(1007218397)
+
+hours <- c('8pm', '9pm', '10pm', '11pm', '12am', '1am')
+
+
+simulated_data <- data.frame(
+  hours,
+  b1 = runif(6, 5, 45), 
+  b2 = runif(6, 10, 50) 
+
+
+print(simulated_data)
+
+
+all(hours %in% simulated_data$hours)
+
+max(simulated_data$b1) <= 50 & min(simulated_data$b1) >= 5
+max(simulated_data$b2) <= 50 & min(simulated_data$b2) >= 10
+
+is.numeric(simulated_data$b1)
+is.numeric(simulated_data$b2)
+is.character(simulated_data$hours)
